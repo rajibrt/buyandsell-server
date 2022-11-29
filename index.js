758a280c4628edc6fdac4ad20c5cc00dafde0bd4
rajibrt <rajibrt@gmail.com>
2022-11-29 08:08:36 +0600

    payment api updated with paid status

--- index.js
+++ index.js
@@ -59,6 +59,15 @@ async function run() {
         app.post('/payments', async (req, res) => {
             const payment = req.body;
             const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await bookedCollection.updateOne(filter, updatedDoc)
             res.send(result);
         })
 
